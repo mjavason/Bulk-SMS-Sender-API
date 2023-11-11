4d03c1e8333820cb96917266848495591adcae6a
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2023-11-11 19:33:14 +0100

    Add empty Twilio and Vcard modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TwilioModule } from './twilio/twilio.module';
+import { VcardModule } from './vcard/vcard.module';
 
 @Module({
-  imports: [],
+  imports: [TwilioModule, VcardModule],
   controllers: [AppController],
   providers: [AppService],
 })
